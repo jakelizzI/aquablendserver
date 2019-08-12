da83c8470ee03b4a8e475a53acc8a08d5934af3f
jakelizzI <jakelizzI@gmail.com>
2019-08-13 00:23:44 +0900

    微調整が必要だが、Slackへの通知はできたので一旦コミット

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,13 +1,71 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Button, Grid, TextArea, Label, Form, Header, Input, Icon, Divider } from 'semantic-ui-react';
 
 import Layout from '../layouts/baseLayout';
 
+const sendMail = () => {
+
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  const data = {'text' : email + ' | ' + message}
+  const url = 'https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+  fetch(url, {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res => {
+    alert('success');
+    console.log(res)
+  })
+  .catch(err => {
+    alert(err);
+    console.log(err);
+  });
+}
+
 const Contact = () => (
   <Layout>
-    <Container textAlign="center">
-      誠意製作中
-    </Container>
+    <Divider />
+    <Form>
+      <Grid>
+        <Grid.Row centered>
+          <Grid.Column computer={12} tablet={16}>
+            <p>
+              ご感想・ご要望、その他ご質問などございましたら下記のフォームより送信してください。{process.env.GATSBY_MY_ENV}|
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column computer={2} tablet={16} textAlign='right'>
+            <Header as='h3'>e-mail</Header>
+          </Grid.Column>
+          <Grid.Column computer={10} tablet={16}>
+            <Input iconPosition='left' placeholder='e-mail'>
+              <Icon name='at' />
+              <input id='email' type='email' size='64' />
+            </Input>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column computer={2} tablet={16} textAlign='right'>
+            <Header as='h3'>message</Header>
+          </Grid.Column>
+          <Grid.Column computer={10} tablet={16}>
+            <TextArea id='message' placeholder='tell us something...' />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column computer={12} tablet={16} textAlign='right'>
+            <Button onClick={sendMail}>Send</Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Form>
   </Layout>
 );
 
