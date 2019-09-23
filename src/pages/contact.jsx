7525460abb61f0593d5ed82f4fcbd225f7f58334
jakelizzI <jakelizzI@gmail.com>
2019-09-23 21:48:28 +0900

    mobile tablet 対応

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -38,7 +38,10 @@ const Contact = () => {
       <Transition visible={visibility}>
         <Message error>
           <Message.Header>メッセージの送信に失敗しました</Message.Header>
-          <p>エラー：{errorMessage}</p>
+          <p>
+            エラー：
+            {errorMessage}
+          </p>
         </Message>
       </Transition>
       <Form onSubmit={sendMessage}>
@@ -54,8 +57,11 @@ const Contact = () => {
             </Grid.Column>
           </Grid.Row>
           <Grid.Row centered>
-            <Grid.Column computer={2} tablet={16} textAlign="right">
+            <Grid.Column computer={2} textAlign="right" only="computer">
               <Popup content="必須項目です" position="top right" trigger={<Header as="h3">e-mail*</Header>} />
+            </Grid.Column>
+            <Grid.Column tablet={16} only="mobile tablet">
+              <Popup content="必須項目です" position="top left" trigger={<Header as="h3">e-mail*</Header>} />
             </Grid.Column>
             <Grid.Column computer={10} tablet={16}>
               <Input iconPosition="left" placeholder="e-mail">
@@ -65,8 +71,11 @@ const Contact = () => {
             </Grid.Column>
           </Grid.Row>
           <Grid.Row centered>
-            <Grid.Column computer={2} tablet={16} textAlign="right">
+            <Grid.Column computer={2} textAlign="right" only="computer">
               <Popup content="必須項目です" position="bottom right" trigger={<Header as="h3">message*</Header>} />
+            </Grid.Column>
+            <Grid.Column tablet={16} only="mobile tablet">
+              <Popup content="必須項目です" position="top left" trigger={<Header as="h3">message*</Header>} />
             </Grid.Column>
             <Grid.Column computer={10} tablet={16}>
               <TextArea id="message" placeholder="tell us something..." onChange={e => setMessage(e.target.value)} required />
